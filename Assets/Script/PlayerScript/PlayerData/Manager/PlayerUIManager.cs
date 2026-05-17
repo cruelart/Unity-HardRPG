1c5468108e2023bd9b5447bd0b092eba46a7cfe6
cruelart <gkwlaks0221@naver.com>
2026-05-17 22:35:45 +0900

    Update PlayerStatManager and PlayerToTalManager

---
+++ Assets/Script/PlayerScript/PlayerData/Manager/PlayerUIManager.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    PlayerStatManager playerStatManager; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/PlayerScript/PlayerData/PlayerDB.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerDB
+{
+    public string playerName; // 이름
+    public int level;
+    public int currentHp;
+    public int currentMp;
+    public float currentExp;
+    public float maxExp;
+
+    //플레이어 위치
+    public string currentMap; // 플레이어가 현재 위치하고 있는 맵
+    public float posX;
+    public float posY;
+    public float posZ;
+
+    public List<Stat> stats;
+
+    public PlayerDB()
+    {
+        playerName = "New Player";
+        level = 1;
+        currentExp = 0;
+        maxExp = level * 100;
+        currentMap = "tutorialMap";
+        posX = 0;
+        posY = 1.0f;
+        posZ = 0;
+        stats = new List<Stat>();
+    }
+}
