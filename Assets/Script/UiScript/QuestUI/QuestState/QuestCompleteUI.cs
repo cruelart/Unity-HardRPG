a356a7f3ba5f84da798b7f5e0aff31abff9fc64a
cruelart <gkwlaks0221@naver.com>
2026-08-27 20:38:03 +0900

    Add QuestCompleteUI Script

---
+++ Assets/Script/UiScript/QuestUI/QuestState/QuestCompleteUI.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestCompleteUI : MonoBehaviour
+{
+    [Header("퀘스트 타입 텍스트 수정")]
+    [SerializeField]
+    private TextMeshProUGUI completeText; // 진행중 텍스트 찐하게 만들예정
+
+    [SerializeField]
+    private Color onEnableColor = new Color32(255, 255, 255, 255); // 하얀색
+
+    [SerializeField]
+    private Color onDisableColor = new Color32(147, 147, 147, 255); // 회색
+
+    [SerializeField]
+    private GameObject complete_prefab; // 진행중 프리팹
+
+    private List<QuestContent> completeQuestList = new List<QuestContent>(); // 진행중 퀘스트 리스트
+
+    [SerializeField]
+    private Transform questListTransform; // 퀘스트 리스트ui의 위치(실제 보일 장소) -> content 받아오면 될듯
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        completeText.color = onEnableColor;
+    }
+
+    private void OnDisable()
+    {
+        completeText.color = onDisableColor;
+    }
+
+    public void AddInProgressQuest(QuestData _questData, QuestProgressData _questProgressData)
+    {
+        QuestContent inProgressQuestContent = Instantiate(complete_prefab, questListTransform).GetComponent<QuestContent>();
+        inProgressQuestContent.Init(_questData, _questProgressData);
+
+        completeQuestList.Add(inProgressQuestContent);
+    }
+}
